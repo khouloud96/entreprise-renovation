5c64d0a3d794dd637d6c25e1dabb37731b8effc7
Khouloud Ben Salem <khouloud.bensalem@ensi-uma.tn>
2024-10-10 10:09:30 +0200

    edit next.config.js for global environment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 const withTM = require("next-transpile-modules")(["react-leaflet", "leaflet"]);
 
-module.exports = withTM({
-  // Autres configurations ici
-});
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (PHASE_DEVELOPMENT_SERVER) {
+    return withTM({
+      env: {
+        EMAIL_USER: "anismiss789@gmail.com",
+        EMAIL_PASSWORD: "omvfirifchzhhvgk",
+      },
+    });
+  }
+
+  return withTM({
+    env: {
+      EMAIL_USER: "anismiss789@gmail.com",
+      EMAIL_PASSWORD: "omvfirifchzhhvgk",
+    },
+  });
+};
